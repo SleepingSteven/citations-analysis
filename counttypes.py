b5e5e7cd9749b65b5d5e32aa6751252735d5c425
Stefano Sorrentino <32730851+SleepingSteven@users.noreply.github.com>
2024-01-20 16:32:36 +0100

    Update counttypes.py

--- counttypes.py
+++ counttypes.py
@@ -6,21 +6,21 @@ import xlsxwriter
 from requests import get
 from collections import Counter
 
+def counttypes():
+     pubtype=pd.read_excel('filtered.xlsx', usecols = 'BA')
+     citationsoc = pd.read_excel('filtered.xlsx', usecols = 'BI')
+     citationsscopus = pd.read_excel('filtered.xlsx', usecols = 'BG')
+     pubs=list()
+     for i, row in pubtype.iterrows():
+          if (citationsoc.loc[i]['cited_by_oc']-citationsscopus.loc[i]['cited_by_scopus'])>0:
+               pubs.append(row[0])
 
-pubtype=pd.read_excel('filtered.xlsx', usecols = 'BA')
-citationsoc = pd.read_excel('filtered.xlsx', usecols = 'BI')
-citationsscopus = pd.read_excel('filtered.xlsx', usecols = 'BG')
-pubs=list()
-for i, row in pubtype.iterrows():
-        if (citationsoc.loc[i]['cited_by_oc']-citationsscopus.loc[i]['cited_by_scopus'])>0:
-            pubs.append(row[0])
+     results=dict()
+     lista=Counter(pubs)
+     for element, count in lista.items():
+          percentage=(count/len(pubs))*100
+          results[str(element)]=float(percentage)
 
-results=dict()
-lista=Counter(pubs)
-for element, count in lista.items():
-     percentage=(count/len(pubs))*100
-     results[str(element)]=float(percentage)
-
-print(sorted(results.items(), key=lambda x: x[1]))
+     print(sorted(results.items(), key=lambda x: x[1]))
+     return(sorted(results.items(), key=lambda x: x[1]))
     
-
